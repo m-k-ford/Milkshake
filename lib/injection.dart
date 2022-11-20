c5c7e6ce8140979abaec6926cf0a1a3c907a05ce
Mark <mark91writer@gmail.com>
2022-11-20 12:48:17 +1000

    major versions upgraded

---
+++ lib/injection.dart
@@ -0,0 +1,5 @@
+import 'package:get_it/get_it.dart';
+
+final injection = GetIt.instance;
+
+void setup() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:milkshake/core/constants/colours.dart' as colour;
 import 'package:milkshake/core/constants/strings.dart';
 import 'package:milkshake/features/user/presentation/screens/sign_in_screen.dart';
 import 'package:milkshake/features/user/presentation/screens/home_screen.dart';
 import 'package:milkshake/features/user/presentation/screens/landing_screen.dart';
 import 'package:milkshake/features/user/presentation/screens/sign_up_screen.dart';
+import 'package:milkshake/injection.dart' as injection;
 
 Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MilkshakeApp());
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    Firebase.initializeApp();
+    injection.setup();
+    runApp(const MilkshakeApp());
+  } catch (error) {
+    print('Error running Milkshake app: $error');
+    SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+  }
 }
 
 class MilkshakeApp extends StatelessWidget {
